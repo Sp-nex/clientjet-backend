a2c65a8a8e26cc28b4471f4a80e2920d18905a6b
Sp-nex <nexusever8@gmail.com>
2025-07-08 19:40:54 -0600

    Fix: removed duplicate express declaration

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
 const path = require("path");
-const express = require("express");
-const cors = require("cors"); 
+
 const app = express();
 const PORT = process.env.port||3000;
 const DATA_FILE = path.join(__dirname, 'orders.json');
